3046eb049096f058643d525dec51713fed787142
Vignesh2308m <vicky.manoharan2018@gmail.com>
2024-02-29 22:47:30 +0530

    Added reduce

--- activity.go
+++ activity.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type dataset struct {
 	data map[int]string
 }
@@ -8,8 +10,34 @@ func NewDataSet(d map[int]string) *dataset {
 	return &dataset{data: d}
 }
 
-func (d *dataset) apply(fn func(string) string) {
+func (d *dataset) apply(fn func(string) string) *dataset {
 	for k, val := range d.data {
 		d.data[k] = fn(val)
 	}
+
+	return d
+}
+
+func (d *dataset) filter(fn func(string) bool) *dataset {
+	for k, val := range d.data {
+		if !fn(val) {
+			delete(d.data, k)
+		}
+	}
+	return d
+}
+
+func (d *dataset) reduce(fn func(string, string) string) *dataset {
+	res := make(map[int]string)
+
+	fmt.Println(len(d.data))
+
+	for k := range d.data {
+		if k < len(d.data) {
+			res[k] = fn(d.data[k], d.data[k+1])
+		}
+	}
+	d.data = res
+
+	return d
 }
--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
+func letter_check(s string) bool {
+	return strings.Contains(s, "V")
+}
+
+func concat(a string, b string) string {
+	return a + b
+}
+
 func main() {
+
+	start := time.Now()
 
 	sample_data := map[int]string{
 		1: "Vijayanila",
@@ -14,7 +25,8 @@ func main() {
 
 	data := NewDataSet(sample_data)
 
-	data.apply(strings.ToUpper)
+	data.apply(strings.ToUpper).filter(letter_check).reduce(concat)
 
 	fmt.Println(data)
+	fmt.Println(time.Since(start))
 }
